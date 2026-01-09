87bd55e6c0d4c0484120f97761cf06d949adb651
karansakure123git <sakurekaran12@example.com>
2026-01-09 15:38:52 +0530

    frontend changes

--- src/services/admin.service.js
+++ src/services/admin.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/admin';
+const API_URL = `${import.meta.env.VITE_API_BASE_URL}/admin`;
 
 // Get all employees
 export const getAllEmployees = async () => {
--- src/services/leave.service.js
+++ src/services/leave.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/leave';
+const API_URL = `${import.meta.env.VITE_API_BASE_URL}/leave`;
 
 // Apply for leave
 export const applyLeave = async (leaveData) => {
